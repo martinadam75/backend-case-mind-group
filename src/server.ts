4b8bb343b0cce7781f9aa21f4c36acbecdb2d02f
Martin Adam <martin.viana99@gmail.com>
2026-05-17 19:36:49 -0300

    feat: create basic express server and environment variables

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+// Carrega as variáveis de ambiente
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+// Middlewares essenciais
+app.use(cors());
+app.use(express.json());
+
+// Teste servidor
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Hello World! API funcional' });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
